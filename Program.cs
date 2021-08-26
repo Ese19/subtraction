048c72540fa2f090bb998714bcb4332ea713c97d
Eseoghene Okoro <enerhen2013@gmail.com>
2021-08-26 10:30:13 -0700

    subtraction

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            int result;
+
+            Console.WriteLine("Enter the first number");
+            firstNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            secondNum = int.Parse(Console.ReadLine());
+
+            result = firstNum - secondNum;
+            Console.WriteLine("The answer is " + result);
+        }
+    }
+}
